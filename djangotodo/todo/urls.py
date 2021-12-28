015976b6236f4689f0029ee037848ca1c543e3c1
sfkse <sfkse5591@gmail.com>
2021-12-28 22:38:50 +0200

    Add edit functionality

--- djangotodo/todo/urls.py
+++ djangotodo/todo/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import listTodo
+from .views import listTodo, editTodo
 
 urlpatterns = [
-    path('', listTodo, name='list')
+    path('', listTodo, name='list'),
+    path('edit/<int:id>', editTodo, name='edit')
 ]
--- djangotodo/todo/views.py
+++ djangotodo/todo/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.http import HttpResponseRedirect
 from .models import Todo
+from django import forms
 from .forms import TodoForm
 
 
@@ -23,3 +24,23 @@ def listTodo(request):
         'form': todo
     }
     return render(request, 'todo/index.html', context)
+
+
+def editTodo(request, id):
+    todo_item = get_object_or_404(Todo, id=id)
+    todo = TodoForm()
+    todo.fields['description'].widget.attrs.update({'placeholder': 'Update...', 'value': todo_item.description})
+    todo.fields['due_date'].widget.attrs.update({'value': todo_item.due_date})
+    context = {
+        'form': todo
+    }
+
+    if request.method == 'POST':
+        todo = TodoForm(request.POST)
+        if todo.is_valid():
+            add_todo = Todo()
+            add_todo.description = todo.cleaned_data['description']
+            add_todo.due_date = todo.cleaned_data['due_date']
+            add_todo.save()
+            return HttpResponseRedirect("/")
+    return render(request, 'todo/edit-todo.html', context)
